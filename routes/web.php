3089f3569a7db61a4e009b8d9417f02405596985
Motherbloods <habibskh06@gmail.com>
2026-04-11 10:39:57 +0700

    feat(jenis-beras): implement CRUD controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JenisBerasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => redirect()->route('dashboard'));
@@ -20,4 +21,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
         return 'Dashboard';
     })->name('dashboard');
+
+    Route::get('/monitoring', function () {
+        return 'Monitoring';
+    })->name('monitoring');
+});
+Route::middleware('role:admin')->group(function () {
+
+    // Master data
+    Route::resource('jenis-beras', JenisBerasController::class)
+        ->parameters([
+            'jenis-beras' => 'jenisBeras'
+        ]);
 });
